b00a70a15b082a4d94d18e9c5a56f35d7408ec7c
Sbahle <sbahlemagubs@gmail.com>
2025-11-18 22:42:27 +0200

    debugged file linking

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 function updateTime() {
   // Tokyo
-  const tokyoDate = document.querySelector("#Tokyo .Date");
-  const tokyoTime = document.querySelector("#Tokyo .Time");
+  let tokyoDate = document.querySelector("#Tokyo .Date");
+  let tokyoTime = document.querySelector("#Tokyo .Time");
 
   tokyoDate.innerHTML = moment().tz("Asia/Tokyo").format("dddd DD MMMM YYYY");
-  tokyoTime.innerHTML = moment()
-    .tz("Asia/Tokyo")
-    .format("hh:mm:ss <small>A</small>");
+  tokyoTime.innerHTML =
+    moment().tz("Asia/Tokyo").format("hh:mm:ss") +
+    " <small>" +
+    moment().tz("Asia/Tokyo").format("A") +
+    "</small>";
 
   // Paris
-  const parisDate = document.querySelector("#Paris .Date");
-  const parisTime = document.querySelector("#Paris .Time");
+  let parisDate = document.querySelector("#Paris .Date");
+  let parisTime = document.querySelector("#Paris .Time");
 
   parisDate.innerHTML = moment().tz("Europe/Paris").format("dddd DD MMMM YYYY");
-  parisTime.innerHTML = moment()
-    .tz("Europe/Paris")
-    .format("hh:mm:ss <small>A</small>");
+  parisTime.innerHTML =
+    moment().tz("Europe/Paris").format("hh:mm:ss") +
+    " <small>" +
+    moment().tz("Europe/Paris").format("A") +
+    "</small>";
 
   // Los Angeles
-  // (Space must be escaped with \\ )
-  const laDate = document.querySelector("#Los\\ Angeles .Date");
-  const laTime = document.querySelector("#Los\\ Angeles .Time");
+  let laDate = document.querySelector("#Los\\ Angeles .Date");
+  let laTime = document.querySelector("#Los\\ Angeles .Time");
 
   laDate.innerHTML = moment()
     .tz("America/Los_Angeles")
     .format("dddd DD MMMM YYYY");
-  laTime.innerHTML = moment()
-    .tz("America/Los_Angeles")
-    .format("hh:mm:ss <small>A</small>");
+  laTime.innerHTML =
+    moment().tz("America/Los_Angeles").format("hh:mm:ss") +
+    " <small>" +
+    moment().tz("America/Los_Angeles").format("A") +
+    "</small>";
 }
+
 function updateCity(event) {
-  const cityTimeZone = event.target.value;
+  let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
 
   if (!cityTimeZone) return; // if user selects "Select a city..", do nothing
 
-  const cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 
-  const cityElement = document.querySelector(".City");
+  let cityElement = document.querySelector(".City");
 
   cityElement.innerHTML = `
     <div>
